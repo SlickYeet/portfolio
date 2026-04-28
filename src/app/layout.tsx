545ef5be357192ac06ccd34c9b89255e296ec8c2
SlickYeet <lasse@lx2.dev>
2026-04-28 16:33:01 -0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+
+import "@/styles/globals.css"
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+})
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+})
+
+export const metadata: Metadata = {
+  description: "My portfolio",
+  title: "Lasse Lammers",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta content="Lasse" name="apple-mobile-web-app-title" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
